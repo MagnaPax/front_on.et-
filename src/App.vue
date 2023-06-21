71aa067d5a73ee9ffcb7a5eb580cec064c86728f
ChunHee Han <chunhee.han@outlook.com>
2023-06-21 13:51:18 +0900

    feat: 모든 파일 올리기

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import ToolBar from './components/ToolBar.vue'
+</script>
+
+<template>
+  <ToolBar />
+  <RouterView />
+</template>
