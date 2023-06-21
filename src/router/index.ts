d374d38a07b31e752ae5387234c586f55e40d378
ChunHee Han <chunhee.han@outlook.com>
2023-06-21 14:10:38 +0900

    feat: 라우터 정리

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import QuestionView from '../views/QuestionView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,19 +12,6 @@ const router = createRouter({
       path: '/home',
       name: 'home',
       component: HomeView
-    },
-    {
-      path: '/question',
-      name: 'question',
-      component: QuestionView
-    },
-    {
-      path: '/answer',
-      name: 'answer',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AnswerView.vue')
     }
   ]
 })
